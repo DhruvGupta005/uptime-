e614cc559bf4623dad0d00dce0e32cb4dbbf98aa
Dhruv Gupta <dhruvgupta5april@gmail.com>
2025-11-20 19:01:07 +0530

    Updated files

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-  function middleware(req) {
+  function proxy(req) {
     return NextResponse.next();
   },
   {
@@ -15,5 +15,4 @@ export default withAuth(
 export const config = {
   matcher: ["/dashboard/:path*", "/monitors/:path*", "/incidents", "/settings"],
 };
-
 
